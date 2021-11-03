a9f1895cfffaf1f2128d104b0ab943c2bb91489a
Дамир Аминов <1damiraminov@gmail.com>
2021-11-03 20:00:40 +0500

    fixed typo mistakes

--- app/email.py
+++ app/email.py
@@ -17,5 +17,5 @@ def send_password_reset_email(user):
               recipients=[user.email],
               text_body=render_template('email/reset_password.txt',
                                         user=user, token=token),
-              html_body=render_template('email/reset_password.html'),
-              user=user, token=token)
+              html_body=render_template('email/reset_password.html',
+                                        user=user, token=token))
