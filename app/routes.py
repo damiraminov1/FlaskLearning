01e8fb7792c611cdb37292cfd2bdf19df1d80525
Дамир Аминов <1damiraminov@gmail.com>
2021-11-09 15:06:27 +0500

    added async email

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
